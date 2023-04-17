b983540611646c1dd44791e838cbba794982415b
Harrison Dutson <totodutson@gmail.com>
2023-04-17 10:36:43 -0600

    Update

--- findTheRange.js
+++ findTheRange.js
@@ -3,14 +3,14 @@ let array = [28, 43, -12, 30, 4, 0, -36]
 
 // Write your solution below:
 
-let smallestNumber = 0;
-let largestNumber = 0;
+let smallestNumber = array[i];
+let largestNumber = array[i];
 
 for (i = 0; i < array.length; i++){
-    if (smallestNumber > array[i]){
+    if (array[i] < smallestNumber){ 
         smallestNumber = array[i]
     }
-    if (largestNumber < array[i]){
+    if (array[i] > largestNumber){
         largestNumber = array[i]
     }
 }
